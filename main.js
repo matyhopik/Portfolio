b2b0aecc887a5e051de6a7e45aa1fc2e197ead26
xhopp <xhopp@ssemi.cz>
2023-02-24 19:08:45 +0100

    footer change, side links, titles

--- main.js
+++ main.js
@@ -24,4 +24,46 @@ menuLinks.forEach(link => {
             header.toggleAttribute("data-overlay");
         }
     });
+});
+
+const sideSocials = document.querySelector('.main-socials');
+const sideMail = document.querySelector('.main-email');
+
+const footerHeight = document.querySelector('.primary-footer').offsetHeight;
+var visited = false;
+var loaded = false;
+
+if (scrollY == 0) {
+    sideSocials.setAttribute("data-visible", "");
+    sideMail.setAttribute("data-visible", "");
+}
+
+window.addEventListener('scroll', function() {
+
+    if (window.scrollY > 500) {
+        if (navigation.hasAttribute("data-visible")) {
+            navButton.setAttribute("aria-expanded", false);
+            navigation.toggleAttribute("data-visible");
+            header.toggleAttribute("data-overlay");
+        }
+    }
+
+    if (window.scrollY + window.innerHeight > document.body.offsetHeight - footerHeight) {
+        if (loaded == true) {
+            sideSocials.setAttribute("data-visible", "false");
+            sideMail.setAttribute("data-visible", "false");
+        }
+        loaded = false;
+        visited = true;
+    } else if (window.scrollY + window.innerHeight < document.body.offsetHeight - footerHeight) {
+        sideSocials.setAttribute("data-visible", "");
+        sideMail.setAttribute("data-visible", "");
+        if (visited == true) {
+            sideSocials.setAttribute("data-visible", "visited");
+            sideMail.setAttribute("data-visible", "visited");
+        }
+        loaded = true;
+    }
+    console.log(loaded)
+    console.log(visited)
   });
