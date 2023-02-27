1b37950d7ece151df3ab2f931fc3719e5cb03cdd
xhopp <xhopp@ssemi.cz>
2023-02-28 00:36:38 +0100

    test

--- js/mail.js
+++ js/mail.js
@@ -1,2 +1,27 @@
+const form = document.querySelector('.contact-form');
+const mailsentbox = document.querySelector('.mail-sent-box');
+
+function sendMsg(e) {
+    console.log('test')
+    e.preventDefault();
+    const subject = document.querySelector('.subject'),
+            email = document.querySelector('.email'),
+            message = document.querySelector('.message');
+            body = '<h2>' + email.value + '</h2>' + '<br/>' + message.value;
+
+    Email.send({
+        SecureToken : "27912f6a-24b8-48af-aa0e-14e4236d9b04",
+        To : 'hoppmatej@gmail.com',
+        From : 'mailserverhoppportfolio@gmail.com',
+        Subject : subject.value,
+        Body : body.toString()
+    }).then(
+        window.scrollTo(0, 0),
+        mailsentbox.classList.add('visible'),
+        setTimeout(() => {
+            mailsentbox.classList.remove('visible')
+        }, 3000)
+    );
+}
 
 form.addEventListener('submit', sendMsg);
