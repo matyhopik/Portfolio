8d8742a1a2474177e21865a27d8c2a43c43f409a
xhopp <xhopp@ssemi.cz>
2023-02-28 00:51:28 +0100

    final try

--- js/mail.js
+++ js/mail.js
@@ -8,7 +8,7 @@ function sendMsg(e) {
             email = document.querySelector('.email'),
             message = document.querySelector('.message');
             securityMessage = message.value.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-            body = '<h2>' + email.value + '</h2>' + '<br/>' + encodeURI(message.value);
+            body = '<h2>' + email.value + '</h2>' + '<br/>' + securityMessage;
 
     Email.send({
         SecureToken : "27912f6a-24b8-48af-aa0e-14e4236d9b04",
