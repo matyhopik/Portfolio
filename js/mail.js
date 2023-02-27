549327c87812622ce58a171d065c70a66dab607e
xhopp <xhopp@ssemi.cz>
2023-02-28 00:44:35 +0100

    mail injection protection

--- js/mail.js
+++ js/mail.js
@@ -7,14 +7,14 @@ function sendMsg(e) {
     const subject = document.querySelector('.subject'),
             email = document.querySelector('.email'),
             message = document.querySelector('.message');
-            body = '<h2>' + email.value + '</h2>' + '<br/>' + message.value;
+            body = '<h2>' + encodeURI(email.value) + '</h2>' + '<br/>' + message.value;
 
     Email.send({
         SecureToken : "27912f6a-24b8-48af-aa0e-14e4236d9b04",
         To : 'hoppmatej@gmail.com',
         From : 'mailserverhoppportfolio@gmail.com',
         Subject : subject.value,
-        Body : body.toString()
+        Body : body
     }).then(
         window.scrollTo(0, 0),
         mailsentbox.classList.add('visible'),
